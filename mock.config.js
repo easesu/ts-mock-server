7ef781678e74e9ad5001feb3e5f5841a30eb6197
easesu <easesu@outlook.com>
2023-03-10 03:46:51 +0800

    feat: 初始项目

---
+++ mock.config.js
@@ -0,0 +1,26 @@
+const { resolve } = require('path');
+
+const successTemplate = `
+{
+  "errcode": 0,
+  "data": $data
+}
+`;
+
+const errorTemplate = `
+{
+  "errcode": 999999,
+  "errmsg": $message,
+}
+`;
+
+
+module.exports = {
+  port: 9876,
+  dir: resolve(__dirname, './mock'),
+  tsRoot: __dirname,
+  template: {
+    success: successTemplate,
+    error: errorTemplate
+  }
+}
