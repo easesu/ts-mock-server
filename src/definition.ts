ab3a8ca48196cd8d6de858c725b0285da06f39d9
easesu <easesu@outlook.com>
2023-03-13 15:35:49 +0800

    feat: schema支持缓存解析的结果

--- src/definition.ts
+++ src/definition.ts
@@ -2,6 +2,7 @@ export interface MockConfig {
   port: number;
   dir: string;
   tsRoot: string;
+  cacheSchema?: boolean;
   template: {
     success: string;
     error: string;
--- src/schema.ts
+++ src/schema.ts
@@ -1,6 +1,6 @@
 import { pathExists } from 'fs-extra';
 import { resolve } from 'path';
-import { getProgramFromFiles, buildGenerator } from 'typescript-json-schema';
+import { getProgramFromFiles, buildGenerator, Definition } from 'typescript-json-schema';
 import configure from './configure';
 
 const buildDefinitionFilePath = (requestPath: string) => {
@@ -18,7 +18,7 @@ const definitionFileExists = async (filePath: string) => {
   return await pathExists(filePath);
 }
 
-const generateSchame = (file: string, symbol: string) => {
+const generateSchameByFile = (file: string, symbol: string) => {
   const program = getProgramFromFiles([file], {}, configure.get('tsRoot'));
   const generator = buildGenerator(program, {
     required: true
@@ -26,7 +26,7 @@ const generateSchame = (file: string, symbol: string) => {
   return generator.getSchemaForSymbol(symbol);
 }
 
-const generate = async (method: string, requestPath: string) => {
+const generateSchemaByRequest = async (method: string, requestPath: string) => {
   const filePath = buildDefinitionFilePath(requestPath);
   const fileFullPath = resolve(configure.get('dir'), filePath);
   const exists = await definitionFileExists(fileFullPath);
@@ -34,13 +34,50 @@ const generate = async (method: string, requestPath: string) => {
     throw new Error('file not exist');
   }
   
-  const schema = generateSchame(fileFullPath, method.toLowerCase());
+  const schema = generateSchameByFile(fileFullPath, method.toLowerCase());
   if (!schema) {
     throw new Error('definition not exist');
   }
 
   return schema;
 };
+
+const schemaCache: {
+  [path: string]: {
+    [method: string]: Definition;
+  }
+} = {};
+
+const pickFromCache = (method: string, requestPath: string) => {
+  if (schemaCache[requestPath] && schemaCache[requestPath][method]) {
+    return schemaCache[requestPath][method];
+  }
+  return null;
+};
+
+const saveToCache = (method: string, requestPath: string, schema: Definition) => {
+  if (!schemaCache[requestPath]) {
+    schemaCache[requestPath] = {};
+  }
+  schemaCache[requestPath][method] = schema;
+};
+
+const generate = async (method: string, requestPath: string) => {
+  const cacheSchema = configure.get('cacheSchema');
+  let res: Definition;
+  if (cacheSchema) {
+    res = pickFromCache(method, requestPath);
+    if (res) {
+      return res;
+    }
+  }
+  res = await generateSchemaByRequest(method, requestPath);
+  if (cacheSchema) {
+    saveToCache(method, requestPath, res);
+  }
+  return res;
+};
+
 
 export default {
   generate
